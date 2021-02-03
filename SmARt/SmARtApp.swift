54933d1428756a46eb0a7e86c8367ae8bb27a30f
Ivan Zavadsky <i.zavadsky@itransition.com>
2021-02-03 12:05:11 +0300

    init

---
+++ SmARt/SmARtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmARtApp.swift
+//  SmARt
+//
+//  Created by MacBook on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SmARtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
